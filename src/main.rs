173885706052b966aeba118f128e9e87d19d8003
Fanisus <flasterDB@gmail.com>
2022-12-10 20:49:38 +0530

    Switch to crossbeam

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
 // Imports //
+use crossbeam;
+use crossbeam::channel::Receiver;
+use crossbeam::channel::Sender;
 use rand::Rng;
 use std::fs;
 use std::io;
-use std::sync::mpsc;
-use std::sync::mpsc::Receiver;
-use std::sync::mpsc::Sender;
 use std::thread;
 use std::time;
 use tokio;
 // MAIN //
 // #[tokio::main]
 fn main() {
-    let (MainTx, ThreadRx) = mpsc::channel();
-    let (ThreadTx, MainRx) = mpsc::channel();
+    let (tx, rx) = crossbeam::channel::unbounded();
+    let tx_thread: Sender<threads::Message> = tx.clone();
+    let rx_thread: Receiver<threads::Message> = rx.clone();
+    let rxc = rx.clone();
 
-    threads::create_thread(threads::ThreadType::Reader, ThreadTx, ThreadRx);
-
-    let x: String = MainRx.recv().unwrap();
-    println!("Data from MAin {}", x);
+    threads::create_thread(threads::ThreadType::Reader, tx_thread, rx_thread);
+    listen(rxc)
 }
-
+fn listen(rx: Receiver<threads::Message>) {
+    let x = rx.recv().unwrap();
+    let y = x.data;
+    println!("Data from MAin {}", y);
+    listen(rx);
+}
 mod threads {
+    use crossbeam;
     use std::sync::mpsc;
     use std::thread;
+    use std::thread::ThreadId;
     use std::time;
 
     pub enum ThreadType {
@@ -30,18 +37,42 @@ mod threads {
         Writer,
         Connection,
     }
+    pub struct Message {
+        thread_id: ThreadId,
+        query: String,
+        data: Data
+    }
+    pub enum Data {
+        String(String),
+        Integer(i128)
+    }
+    fn get_type_of<T>(_: &T) -> String {
+        std::any::type_name::<T>().to_string()
+    }
     pub fn create_thread(
         thread_type: ThreadType,
-        ThreadTx: mpsc::Sender<String>,
-        ThreadRx: mpsc::Receiver<String>,
+        ThreadTx: crossbeam::channel::Sender<Message>,
+        ThreadRx: crossbeam::channel::Receiver<Message>,
     ) {
         match thread_type {
             ThreadType::Reader => {
-                thread::spawn(|| {
+                thread::spawn(move || {
                     thread::sleep(time::Duration::from_millis(1000));
-                    let val = String::from("hi");
+                    let result = String::from("hi");
                     println!("Thread {:?}", thread::current().id().to_owned());
-                    ThreadTx.send(val).unwrap();
+                    match get_type_of(&result).as_str() {
+                        "String" => {
+                            let data = Message {
+                                thread_id: thread::current().id().to_owned(),
+                                data: Data::String(result),
+                                query: "hi".to_string()
+                            };
+                            ThreadTx.send(data).unwrap();
+                        },
+
+                    }
+                    for i in 0..10 {
+                    }
                 });
             }
             _ => {}
