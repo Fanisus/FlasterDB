b78b7bd365cb196aeee0051db8dca4c928d3ea26
Fanisus <flasterDB@gmail.com>
2022-12-10 17:22:24 +0530

    Crossbeam not implemented

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,50 @@
 // Imports //
 use rand::Rng;
+use std::fs;
 use std::io;
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
+use std::thread;
 use std::time;
-
+use tokio;
 // MAIN //
+// #[tokio::main]
 fn main() {
-    loop {
-        let mut input = String::new();
-        // io::stdin().read_line(&mut input).unwrap();
-        // println!("{input}");
-        let mut input = "select * from".split(" ");
-        let input: Vec<&str> = input.collect();
-        // let mut first_arg = input.get(0).unwrap().trim().to_owned();
-        print!("{:?}", input);
-        if (input.get(0).unwrap().trim().eq_ignore_ascii_case("SELECT")) {
-            println!("{}", input.get(0).unwrap().trim());
-            if (input.get(1).unwrap().trim().eq_ignore_ascii_case("*")) {
-                println!("{}", input.get(1).unwrap().trim());
-                if (input.get(2).unwrap().trim().eq_ignore_ascii_case("FROM")) {
-                    println!("{}", input.get(2).unwrap().trim());
-                    print!();
-                    println!("Ok")
-                }
+    let (MainTx, ThreadRx) = mpsc::channel();
+    let (ThreadTx, MainRx) = mpsc::channel();
+
+    threads::create_thread(threads::ThreadType::Reader, ThreadTx, ThreadRx);
+
+    let x: String = MainRx.recv().unwrap();
+    println!("Data from MAin {}", x);
+}
+
+mod threads {
+    use std::sync::mpsc;
+    use std::thread;
+    use std::time;
+
+    pub enum ThreadType {
+        Reader,
+        Writer,
+        Connection,
+    }
+    pub fn create_thread(
+        thread_type: ThreadType,
+        ThreadTx: mpsc::Sender<String>,
+        ThreadRx: mpsc::Receiver<String>,
+    ) {
+        match thread_type {
+            ThreadType::Reader => {
+                thread::spawn(|| {
+                    thread::sleep(time::Duration::from_millis(1000));
+                    let val = String::from("hi");
+                    println!("Thread {:?}", thread::current().id().to_owned());
+                    ThreadTx.send(val).unwrap();
+                });
             }
-        } else {
-            println!(">>>>> {:?}", input)
+            _ => {}
         }
     }
 }
